70f74dd27a7b3e1a477ef21e9a046b1972577fe5
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-30 09:02:06 -0800

    Write a JavaScript program  convert an array of {}

---
+++ Exercise_5/index.js
@@ -0,0 +1,26 @@
+const JSON_to_CSV = (arr, columns, delimiter = ',') =>
+        [
+          columns.join(delimiter),
+          ...arr.map(obj =>
+            columns.reduce(
+              (acc, key) => `${acc}${!acc.length ? '' : delimiter}"${!obj[key] ? '' : obj[key]}"`,
+              ''
+            )
+          )
+        ].join('\n');
+      
+      console.log(JSON_to_CSV([{ x: 100, y: 200 }, { x: 300, y: 400, z: 500 }, { x: 6 }, { y: 7 }], ['x', 'y']));
+      console.log(JSON_to_CSV([{ x: 100, y: 200 }, { x: 300, y: 400, z: 500 }, { x: 6 }, { y: 7 }], ['x', 'y'], ';'));
+    
+      /* Sample Output
+        x,y
+        "100","200"
+        "300","400"
+        "6",""
+        "","7"
+        x;y
+        "100";"200"
+        "300";"400"
+        "6";""
+        "";"7"
+      */  
