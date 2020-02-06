1082c0790bfa1603a0dff85cc53353ae69abea50
Robert Elias <RobertElias@users.noreply.github.com>
2020-02-05 20:26:28 -0800

    Clone an array

---
+++ Exercise_10/index.js
@@ -0,0 +1,18 @@
+var array_Clone = function(arra1) {
+    return arra1.slice(0);
+       };
+   console.log(array_Clone([1, 2, 4, 0]));
+   console.log(array_Clone([1, 2, [4, 0]]));
+
+////
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
